c49225c9e5d09b41749c019083f8824f40b22b29
Harishdhanukonda <dhanukondaharish12@gmail.com>
2024-09-19 16:08:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function fibonacci(num) {
 // your code here
-    if (num === 1) {
-        return 0;
-    } else if (num === 2) {
-        return 1;
+   if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    // Use dynamic programming for efficiency
+    let fibNums = [0, 1];
+    for (let i = 2; i <= num; i++) {
+      fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
     }
-
-    let a = 0, b = 1;
-    for (let i = 2; i < num; i++) {
-        let temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return a;
-}
+    return fibNums[num];
+  }
 module.exports = fibonacci;
